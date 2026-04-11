9547df39766ef4d90b3b5c447b5f2fcc70d5624b
CodingwithKenny <okunlolaridwanat@gmail.com>
2026-04-11 17:35:25 +0100

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,12 +26,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8PF2SP88QE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8PF2SP88QE'); // Your Google Analytics 4 Measurement ID
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
         <div className="mt-10">
-          <Footer/>
+          <Footer />
         </div>
       </body>
     </html>
